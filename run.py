212b275b9161d381fdf6f6b521d40d5a6289017b
Hope Tracy <hnjoroge64@gmail.com>
2023-03-01 13:59:00 +0000

    Add get_indicator function for user to input their data

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import gspread 
+import gspread
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -10,9 +10,18 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('badware_detective')    
+SHEET = GSPREAD_CLIENT.open('badware_detective')
 
-indicators = SHEET.worksheet('indicators')
 
-data = indicators.get_all_values()
-print(data)
+def get_indicator():
+    """
+    Get the indicator from the user,
+    Indicators to be provided should be domain name,
+    IP address or File Hash
+    """
+
+    data_provided = input("Enter your indicator here:\n")
+    print(f"The data you provided is {data_provided}") 
+
+
+get_indicator()
